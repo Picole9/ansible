7b43c685850117403e70a8e968b52ce4d7033fd6
Ole Specht <ole.specht@ifam.fraunhofer.de>
2025-10-02 12:42:47 +0200

    added bootstrap_local

---
+++ bootstrap_local.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+# detect os
+if [ -f /etc/os-release ]; then
+    . /etc/os-release
+    if [[ -z $ID_LIKE ]]; then
+        OS=$ID
+    else
+        OS=$ID_LIKE
+    fi
+else
+    OS=$(uname -s)
+fi
+echo detected $OS
+
+echo generate locale
+sed -i '/de_DE.UTF-8 UTF-8/s/^#//g' /etc/locale.gen
+sed -i '/en_US.UTF-8 UTF-8/s/^#//g' /etc/locale.gen
+locale-gen
+localectl set-locale LANG=de_DE.UTF-8
+localectl set-keymap de
+
+echo install essentials
+case $OS in
+debian)
+    apt update && sudo apt upgrade -y
+    apt install git -y
+    ;;
+
+arch)
+    paman -Syu
+    pacman -Sy git sudo
+    ;;
+*)
+    echo "$OS not supported"
+    ;;
+esac
+
+echo Do you want to create a new user?
+select yn in "Yes" "No"; do
+    case $yn in
+    Yes)
+        echo " "
+        while read -p "Please enter the username you wish to create : " username; do
+            if [ "x$username" = "x" ]; then
+                echo "Blank username entered. Try again!"
+                username=""
+            elif grep -q "^$username" /etc/passwd; then
+                echo "Username already exists. Try again!"
+                username=""
+            else
+                useradd -m -g users -G wheel -s /bin/bash "$username"
+                echo "%wheel ALL=(ALL) ALL" >/etc/sudoers.d/wheel
+                passwd $username
+                echo on wsl you might want to set the default user in /etc/wsl.conf
+            fi
+        done
+        ;;
+    No)
+        break
+        ;;
+    esac
+done
