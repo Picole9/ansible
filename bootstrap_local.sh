fc729e55ac2a424992cd1d43e783ab26fe9be0e4
Ole Specht <ole.specht@ifam.fraunhofer.de>
2025-10-06 10:55:18 +0200

    proxyconf

--- bootstrap_local.sh
+++ bootstrap_local.sh
@@ -2,14 +2,14 @@
 
 # detect os
 if [ -f /etc/os-release ]; then
-    . /etc/os-release
-    if [[ -z $ID_LIKE ]]; then
-        OS=$ID
-    else
-        OS=$ID_LIKE
-    fi
+  . /etc/os-release
+  if [[ -z $ID_LIKE ]]; then
+    OS=$ID
+  else
+    OS=$ID_LIKE
+  fi
 else
-    OS=$(uname -s)
+  OS=$(uname -s)
 fi
 echo detected $OS
 
@@ -23,41 +23,41 @@ localectl set-keymap de
 echo install essentials
 case $OS in
 debian)
-    apt update && sudo apt upgrade -y
-    apt install git -y
-    ;;
-
+  apt update && apt upgrade -y
+  apt install git -y
+  ;;
 arch)
-    paman -Syu
-    pacman -Sy git sudo
-    ;;
+  paman -Syu
+  pacman -Sy git sudo
+  ;;
 *)
-    echo "$OS not supported"
-    ;;
+  echo "$OS not supported"
+  ;;
 esac
+echo 'Defaults env_keep += "*_proxy *_PROXY"' >/etc/sudoers.d/proxy
 
 echo Do you want to create a new user?
 select yn in "Yes" "No"; do
-    case $yn in
-    Yes)
-        echo " "
-        while read -p "Please enter the username you wish to create : " username; do
-            if [ "x$username" = "x" ]; then
-                echo "Blank username entered. Try again!"
-                username=""
-            elif grep -q "^$username" /etc/passwd; then
-                echo "Username already exists. Try again!"
-                username=""
-            else
-                useradd -m -g users -G wheel -s /bin/bash "$username"
-                echo "%wheel ALL=(ALL) ALL" >/etc/sudoers.d/wheel
-                passwd $username
-                echo on wsl you might want to set the default user in /etc/wsl.conf
-            fi
-        done
-        ;;
-    No)
-        break
-        ;;
-    esac
+  case $yn in
+  Yes)
+    echo " "
+    while read -p "Please enter the username you wish to create : " username; do
+      if [ "x$username" = "x" ]; then
+        echo "Blank username entered. Try again!"
+        username=""
+      elif grep -q "^$username" /etc/passwd; then
+        echo "Username already exists. Try again!"
+        username=""
+      else
+        useradd -m -g users -G wheel -s /bin/bash "$username"
+        echo "%wheel ALL=(ALL) ALL" >/etc/sudoers.d/wheel
+        passwd $username
+        echo on wsl you might want to set the default user in /etc/wsl.conf
+      fi
+    done
+    ;;
+  No)
+    break
+    ;;
+  esac
 done
